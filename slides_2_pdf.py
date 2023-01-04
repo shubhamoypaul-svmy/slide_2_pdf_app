1f5eea7860b1a4f51baa857d8d00052f2beee4b3
shubhamoy paul <83281071+shubhamoypaul-svmy@users.noreply.github.com>
2023-01-04 19:57:03 +0530

    Update slides_2_pdf.py

--- slides_2_pdf.py
+++ slides_2_pdf.py
@@ -67,12 +67,12 @@ for uploaded_file in uploaded_files:
      name=uploaded_file.name
      #pdfFileObj = open(io.BytesIO(bytes_data))
      fun(io.BytesIO(bytes_data),5,name)
-     with open(name.split(".")[0]+'_new.pdf', "rb") as fp:
+     with open(name+'_new.pdf', "rb") as fp:
         btn = st.download_button(
             label="Download PDF",
             data=fp,
-            file_name=name.split(".")[0]+'_new.pdf'
+            file_name=name+'_new.pdf'
             )
-     os.remove(name.split(".")[0]+'_new.pdf')
+     os.remove(name+'_new.pdf')
     
      
